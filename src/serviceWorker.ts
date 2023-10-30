11957eb8ce6f1c908417eae7fe6e3936dcad704f
Artem Manushenkov <artemmanusenkov@gmail.com>
2023-10-31 00:18:23 +0600

    beautify service worker

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,7 +1,7 @@
 const CATALOG = "catalog";
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  if (changeInfo.url && changeInfo.url.includes(CATALOG)) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.url?.includes(CATALOG)) {
     chrome.tabs.sendMessage(tabId, {
       message: "tabUpdated",
       url: changeInfo.url,
